8312496b25a4b7924a60b581fcf1a56a6c00a1a8
WebersIO <muzammilmetar82@gmail.com>
2025-06-05 21:59:43 +0530

    completed

--- src/components/PlayingVideo.jsx
+++ src/components/PlayingVideo.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchdata } from "../utils/rapidapi";
+import ReactPlayer from "react-player";
 
 const PlayingVideo = () => {
-  return (
-    <div className='mt-28'>PlayingVideo</div>
-  )
-}
+  const [video, setVideo] = useState();
+  const [relatedVideo, setRelatedVideo] = useState();
+  const { id } = useParams();
 
-export default PlayingVideo
+  useEffect(() => {
+    fetchVideoDetails();
+  }, [id]);
+
+  const fetchVideoDetails = () => {
+    fetchdata(`video/details/?id=${id}`).then((res) => {
+      console.log(res);
+      setVideo(res);
+    });
+  };
+  return (
+    <div className="mt-28 ">
+      <div>
+        <div>
+          <div>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              height="100%"
+              width="100%"
+              controls
+              style={{ backgroundColor: "#000000" }}
+              playing={true}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlayingVideo;
